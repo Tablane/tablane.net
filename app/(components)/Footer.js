2d817627f9d66b00b23a38520923267d3c788a90
MarconLP <13001502+MarconLP@users.noreply.github.com>
2022-11-12 21:05:19 +0100

    add left and right margin on footer elements

--- app/(components)/Footer.js
+++ app/(components)/Footer.js
@@ -2,15 +2,15 @@ export default function Footer() {
   return (
     <div className="flex flex-row h-[120px] justify-center">
       <div className="w-[1048px] h-full flex justify-between items-center flex-row text-sm">
-        <div>© 2022 TaskBoard</div>
-        <div>
-          <span className="mr-8 cursor-pointer text-sm text-[#666] hover:text-black">
+        <div className="mx-8">© 2022 TaskBoard</div>
+        <div className="mx-8">
+          <span className="ml-8 cursor-pointer text-sm text-[#666] hover:text-black">
             Privacy Policy
           </span>
-          <span className="mr-8 cursor-pointer text-sm text-[#666] hover:text-black">
+          <span className="ml-8 cursor-pointer text-sm text-[#666] hover:text-black">
             Terms and Conditions
           </span>
-          <span className="mr-8 cursor-pointer text-sm text-[#666] hover:text-black">
+          <span className="ml-8 cursor-pointer text-sm text-[#666] hover:text-black">
             Contact
           </span>
         </div>
