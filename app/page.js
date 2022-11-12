02c0063467a54b0ca3f72e36b319823276f3ea8a
MarconLP <13001502+MarconLP@users.noreply.github.com>
2022-11-12 15:37:50 +0100

    change pages/ to app/ routing

---
+++ app/page.js
@@ -0,0 +1,11 @@
+export default function HomePage() {
+  return (
+    <div>
+      <div className="flex bg-red-200">
+        <span>logo</span>
+        <span>nav items</span>
+        <span>open app</span>
+      </div>
+    </div>
+  );
+}
