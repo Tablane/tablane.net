8a0765f66bab316945ef43f5dc90c8c0c8bb989a
MarconLP <13001502+MarconLP@users.noreply.github.com>
2022-11-12 16:42:24 +0100

    setup navbar

--- app/page.js
+++ app/page.js
@@ -1,15 +1,29 @@
-import "../styles/globals.css";
+import Image from "next/image";
 
 export default function HomePage() {
   return (
     <div>
-      <div className="h-[64px] flex">
-        <div className="w-[1048px] m-auto flex justify-between px-[24px]">
-          <span>logo</span>
-          <span>nav items</span>
-          <span>open app</span>
+      <div className="h-[64px] flex sticky top-0 backdrop-blur-sm backdrop-saturate-200">
+        <div className="w-[1048px] m-auto flex justify-between px-[24px] items-center">
+          <Image
+            className="p-2 cursor-pointer"
+            src="/android-chrome-192x192.png"
+            alt="logo"
+            width={42}
+            height={42}
+          />
+          <div>
+            <span>Features</span>
+            <span>Templates</span>
+            <span>Integrations</span>
+            <span>Enterprise</span>
+          </div>
+          <button className="h-[32px] p-2 bg-black rounded-[5px] border-black border-solid border text-sm leading-3 text-white hover:bg-white hover:text-black transition">
+            Open App
+          </button>
         </div>
       </div>
+      <div className="bg-red-200 text-center">blabla</div>
     </div>
   );
 }
