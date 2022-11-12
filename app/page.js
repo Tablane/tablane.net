2af46d2d630bc75cc8c328c2e8d21361cf6e360e
MarconLP <13001502+MarconLP@users.noreply.github.com>
2022-11-12 17:44:29 +0100

    add hero section

--- app/page.js
+++ app/page.js
@@ -31,7 +31,19 @@ export default function HomePage() {
           </button>
         </div>
       </div>
-      <div className="bg-red-200 text-center">blabla</div>
+      <div className="text-center my-[8rem]">
+        <div className="flex flex-col items-center">
+          <span className="py-3 text-2xl font-bold text-[#191919]">
+            Next Generation
+          </span>
+          <span className="py-3 text-7xl font-black text-[#191919]">
+            Productivity
+          </span>
+          <button className="mt-[64px] font-normal h-[50px] w-[200px] p-2 bg-black rounded-[5px] border-black border-solid border leading-3 text-white hover:bg-white hover:text-black transition">
+            Get Started
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
