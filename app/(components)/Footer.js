8c7206bed004b47ba9fb2f26c09a5338cf9591ea
MarconLP <13001502+MarconLP@users.noreply.github.com>
2022-11-12 21:22:17 +0100

    apply same color to copyright text

--- app/(components)/Footer.js
+++ app/(components)/Footer.js
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <div className="flex flex-row h-[120px] justify-center">
       <div className="w-[1048px] h-full flex justify-between items-center flex-row text-sm">
-        <div className="mx-8">© 2022 TaskBoard</div>
+        <div className="mx-8 text-sm text-[#666]">© 2022 TaskBoard</div>
         <div className="mx-8">
           <span className="ml-8 cursor-pointer text-sm text-[#666] hover:text-black">
             Privacy Policy
