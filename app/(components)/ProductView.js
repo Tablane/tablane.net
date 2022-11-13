0adea1b63fb3e97aff7430710456457f94ce4616
MarconLP <13001502+MarconLP@users.noreply.github.com>
2022-11-13 13:33:57 +0100

    make product image responsive

--- app/(components)/ProductView.js
+++ app/(components)/ProductView.js
@@ -1,14 +1,13 @@
 import Image from "next/image";
+import app_board_view from "../../public/app_board_view.png";
 
 export default function ProductView() {
   return (
-    <div className="flex justify-center items-center mb-[100px]">
+    <div className="flex justify-center items-center mb-[100px] px-[50px] lg:px-[150px]">
       <Image
-        className="rounded-xl image-shadow border border-[#cbcdd1]"
-        src="/app_board_view.png"
+        className="rounded-xl product-shadow border border-[#cbcdd1]"
+        src={app_board_view}
         alt="taskboard app board view"
-        width={480 * 2}
-        height={270 * 2}
       />
     </div>
   );
