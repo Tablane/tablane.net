bd032ce2754205787d3d40cfa06b3f908d99a90a
MarconLP <13001502+MarconLP@users.noreply.github.com>
2022-11-13 21:10:58 +0100

    add gradient to hero section text

--- app/(components)/HeroSection.js
+++ app/(components)/HeroSection.js
@@ -5,7 +5,7 @@ export default function HeroSection() {
         <span className="py-3 text-[4vw] sm:text-2xl font-bold text-[#191919]">
           Next Generation
         </span>
-        <span className="py-3 text-[12vw] sm:text-7xl font-black text-[#191919]">
+        <span className="title-text-gradient py-3 text-[12vw] sm:text-7xl font-black text-[#191919]">
           Productivity
         </span>
         <button className="mt-[64px] font-normal h-[50px] w-[200px] p-2 bg-black rounded-[5px] border-black border-solid border leading-3 text-white hover:bg-white hover:text-black transition">
