c22c53bf7a5c80a3de689a250e0d8fbdbbb2d2f4
MarconLP <13001502+MarconLP@users.noreply.github.com>
2022-11-13 17:20:15 +0100

    adjust legal title size on smaller devices

--- app/legal/Legal.js
+++ app/legal/Legal.js
@@ -1,8 +1,10 @@
 export default function Legal({ name, children }) {
   return (
-    <div className="">
+    <div className="mb-[30px] sm:mb-0">
       <div className="flex justify-center">
-        <p className="text-[72px] font-extrabold my-[100px]">{name}</p>
+        <p className="text-[10vw] sm:text-[72px] font-extrabold my-[100px]">
+          {name}
+        </p>
       </div>
       <div className="bg-[#FAFAFA] flex justify-center border-b border-[#cbcdd1]">
         <div className="w-[900px]">{children}</div>
