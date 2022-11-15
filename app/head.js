2349807d374eb4391432da853ac3a152fb124046
MarconLP <13001502+MarconLP@users.noreply.github.com>
2022-11-15 10:06:50 +0100

    uupdate title

--- app/head.js
+++ app/head.js
@@ -1,7 +1,7 @@
 export default function Head() {
   return (
     <>
-      <title>Tavy - The next generation Productivity.</title>
+      <title>Tablane - Next generation Productivity.</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <link rel="icon" href="/favicon.ico" />
     </>
