21a3e1c9615667ce60641bf9ab2f3260d4260220
MarconLP <13001502+MarconLP@users.noreply.github.com>
2022-11-15 14:02:36 +0100

    add animations to hero section

--- app/(components)/HeroSection.js
+++ app/(components)/HeroSection.js
@@ -1,17 +1,65 @@
+"use client";
+
+import { motion } from "framer-motion";
+
+const container = {
+  hidden: {},
+  show: {
+    transition: {
+      delayChildren: 0.05,
+      staggerChildren: 0.1,
+    },
+  },
+};
+
+const item = {
+  hidden: {
+    opacity: 0,
+    y: 100,
+  },
+  show: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 1,
+      ease: "easeInOut",
+    },
+  },
+};
+
+const button = {
+  ...item,
+  show: {
+    ...item.show,
+    transition: {
+      ...item.show.transition,
+      opacity: { delay: 1 },
+    },
+  },
+};
+
 export default function HeroSection() {
   return (
     <div className="text-center mt-[8rem]">
-      <div className="flex flex-col items-center">
-        <span className="py-3 text-[4vw] sm:text-2xl font-bold text-[#191919]">
-          Next Generation
-        </span>
-        <span className="title-text-gradient py-3 text-[12vw] sm:text-7xl font-black text-[#191919]">
+      <motion.div
+        variants={container}
+        initial="hidden"
+        animate="show"
+        className="flex flex-col items-center"
+      >
+        <motion.span variants={item}>Next Generation</motion.span>
+        <motion.span
+          variants={item}
+          className="title-text-gradient py-3 text-[12vw] sm:text-7xl font-black text-[#191919]"
+        >
           Productivity
-        </span>
-        <button className="mt-[64px] font-normal h-[50px] w-[200px] p-2 bg-black rounded-[5px] border-black border-solid border leading-3 text-white hover:bg-white hover:text-black transition">
-          Get Started
-        </button>
-      </div>
+        </motion.span>
+        <motion.div variants={button}>
+          <button className="mt-[64px] font-normal h-[50px] w-[200px] p-2 bg-black rounded-[5px] border-black border-solid border leading-3 text-white hover:bg-white hover:text-black transition">
+            Get Started
+          </button>
+        </motion.div>
+      </motion.div>
     </div>
   );
 }
