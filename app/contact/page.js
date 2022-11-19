7e0efb3f1efc6ea2d751fa539e012c77e84f36a5
MarconLP <13001502+MarconLP@users.noreply.github.com>
2022-11-19 15:54:51 +0100

    add contact page boilerplate

---
+++ app/contact/page.js
@@ -0,0 +1,21 @@
+export default function Contact() {
+  return (
+    <div className="bg-[#FAFAFA]">
+      <div className="text-center">
+        <h1 className="font-bold text-[64px]">We are here to help</h1>
+        <span className="text-[20px]">
+          Please don’t hesitate to get in touch with us if you have a query or
+          want to give some feedback.
+        </span>
+      </div>
+      <div className="p-6">
+        <div>
+          <span>email</span>
+        </div>
+        <div>
+          <span>discord</span>
+        </div>
+      </div>
+    </div>
+  );
+}
