5ab50e66d060b29d4f7545b18703096eb46d6fc3
MarconLP <13001502+MarconLP@users.noreply.github.com>
2022-11-20 20:09:58 +0100

    add different title to page

---
+++ app/contact/head.js
@@ -0,0 +1,11 @@
+export default function Head() {
+  return (
+    <>
+      <title>Contact - Tablane</title>
+      <meta
+        content="We’re here to help with any Tablane related questions."
+        name="description"
+      />
+    </>
+  );
+}
---
+++ app/legal/privacy-policy/head.js
@@ -0,0 +1,7 @@
+export default function Head() {
+  return (
+    <>
+      <title>Privacy Policy - Tablane</title>
+    </>
+  );
+}
