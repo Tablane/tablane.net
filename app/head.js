463e87cbfeb93f407137cddf3977449bbeb813e0
MarconLP <13001502+MarconLP@users.noreply.github.com>
2022-11-20 10:16:47 +0100

    add description meta tag

--- app/head.js
+++ app/head.js
@@ -3,6 +3,10 @@ export default function Head() {
     <>
       <title>Tablane - Next generation Productivity.</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta
+        content="Tablane is a workspace that adapts to your needs. It's as minimal or as powerful as you need it to be."
+        name="description"
+      />
       <link rel="icon" href="/favicon.ico" />
     </>
   );
