f10d878b9e73584893d9abff1db78bceb3c50d1e
MarconLP <13001502+MarconLP@users.noreply.github.com>
2022-12-23 15:51:39 +0100

    improve changelog design with tailwind prose plugin

--- app/changelog/page.tsx
+++ app/changelog/page.tsx
@@ -1,11 +1,12 @@
 import dayjs from "dayjs";
 import Link from "next/link";
 import changelog from "./changelog.json";
+import md from "markdown-it";
 
 export default function Changelog() {
   return (
     <div className="ml-[50px] mr-[20px] flex justify-center">
-      <div className="max-w-[1024px]">
+      <div className="max-w-[1024px] flex-grow">
         <p className="text-[32px] font-semibold pt-[50px] pb-[17px] border-b border-solid border-[#EAEAEA]">
           Changelog
         </p>
@@ -13,9 +14,9 @@ export default function Changelog() {
           {changelog["changelog"].map((change) => (
             <div
               key={change.date}
-              className="flex flex-col sm:flex-row justify-between mb-12"
+              className="flex flex-col md:flex-row justify-between mb-12"
             >
-              <div className="mr-12 flex-1 pb-8 sm:pb-0 h-fit sm:sticky top-[80px]">
+              <div className="mr-12 flex-1 pb-8 md:pb-0 h-fit md:sticky top-[80px]">
                 <Link
                   href={`/changelog/${change.name}`}
                   className="text-2xl font-extrabold text-[#24292f]"
@@ -26,8 +27,13 @@ export default function Changelog() {
                   {dayjs(change.date).format("MMMM DD, YYYY")}
                 </p>
               </div>
-              <div className="grow-[2] lg:grow-[5] basis-0 flex">
-                <p className="">{change.description}</p>
+              <div className="grow-[2] lg:grow-[5] basis-0 flex prose">
+                <div
+                  className="mt-[-20px]"
+                  dangerouslySetInnerHTML={{
+                    __html: md().render(change.description),
+                  }}
+                ></div>
               </div>
             </div>
           ))}
