31fe54100a96c50a2ef763ede962c3064ab40cee
MarconLP <13001502+MarconLP@users.noreply.github.com>
2022-12-23 13:57:22 +0100

    move changelog into changelog.json

--- app/changelog/[change]/page.tsx
+++ app/changelog/[change]/page.tsx
@@ -1,25 +1,14 @@
 import dayjs from "dayjs";
 import Link from "next/link";
+import changelog from "../changelog.json";
+import { notFound } from "next/navigation";
 
-export default function Change() {
-  const change = {
-    name: "Comments on Preview Deployments are now generally available",
-    description:
-      "You can now make Preview Deployments private for free, across all plans.\n" +
-      "\n" +
-      "Shareable Links: Share private Preview Deployments with external collaborators without the need to log in. See docs for limits.\n" +
-      "Vercel Authentication: Team members can log in with their Vercel account to access secure previews.\n" +
-      "Password Protection is now Advanced Deployment Protection and keeps the same price at $150/mo. Pro and Enterprise customers can add on Advanced Deployment Protection, which includes:\n" +
-      "\n" +
-      "Private Production Deployments: Make your production deployment private with the click of a button.\n" +
-      "Protection Bypass for Automation: Allows you to bypass user authentication by using header secret for automated tooling.\n" +
-      "Password Protection: Secure preview and production deployments with a password.\n" +
-      "Enterprise customers will also have access to audit logs allowing them to track who generated a given Sharable Link at what time and from which device.\n" +
-      "\n" +
-      "Check out the documentation to learn more.\n" +
-      "\n",
-    date: 1671736995280,
-  };
+export default function Change({ params }) {
+  const change = changelog["changelog"].find((x) => x.name === params.change);
+
+  if (!change) {
+    notFound();
+  }
 
   return (
     <div className="p-6 lg:p-16">
