2edc9f8a16ca384ca9533b5609ac647facce4459
MarconLP <13001502+MarconLP@users.noreply.github.com>
2022-12-23 14:33:13 +0100

    improve changelog design on large screens

--- app/changelog/[change]/page.tsx
+++ app/changelog/[change]/page.tsx
@@ -11,26 +11,26 @@ export default function Change({ params }) {
   }
 
   return (
-    <div className="p-6 lg:p-16">
-      <Link
-        className="flex mb-[65px] cursor-pointer text-[#666] text-sm hover:text-black"
-        href="/changelog"
-      >
-        ← Back to Changelog
-      </Link>
-      <div className="flex items-center">
-        <div className="inline-flex py-1 px-3 whitespace-no-wrap rounded-full bg-[#eaeaea]">
-          <p className="text-sm">Changelog</p>
+    <div className="flex justify-center">
+      <div className="p-6 lg:p-16 max-w-[1024px]">
+        <Link
+          className="flex mb-[65px] cursor-pointer text-[#666] text-sm hover:text-black"
+          href="/changelog"
+        >
+          ← Back to Changelog
+        </Link>
+        <div className="flex items-center">
+          <div className="inline-flex py-1 px-3 whitespace-no-wrap rounded-full bg-[#eaeaea]">
+            <p className="text-sm">Changelog</p>
+          </div>
+          <p className="ml-4 text-[#666] text-sm">
+            {dayjs(change.date).format("MMMM DD, YYYY")}
+          </p>
         </div>
-        <p className="ml-4 text-[#666] text-sm">
-          {dayjs(change.date).format("MMMM DD, YYYY")}
+        <p className="text-[32px] lg:text-[80px] font-bold leading-none py-12">
+          {change.name}
         </p>
-      </div>
-      <p className="text-[32px] lg:text-[80px] font-bold leading-none py-12 lg:w-[75%]">
-        {change.name}
-      </p>
-      <div className="text-base lg:text-lg lg:w-[75%]">
-        {change.description}
+        <div className="text-base lg:text-lg">{change.description}</div>
       </div>
     </div>
   );
--- app/changelog/page.tsx
+++ app/changelog/page.tsx
@@ -4,32 +4,34 @@ import changelog from "./changelog.json";
 
 export default function Changelog() {
   return (
-    <div className="ml-[50px] mr-[20px]">
-      <p className="text-[32px] font-semibold pt-[50px] pb-[17px] border-b border-solid border-[#EAEAEA]">
-        Changelog
-      </p>
-      <div className="mt-8">
-        {changelog["changelog"].map((change) => (
-          <div
-            key={change.date}
-            className="flex flex-col sm:flex-row justify-between mb-12"
-          >
-            <div className="mr-12 flex-1 pb-8 sm:pb-0 h-fit sm:sticky top-[80px]">
-              <Link
-                href={`/changelog/${change.name}`}
-                className="text-2xl font-extrabold text-[#24292f]"
-              >
-                {change.name}
-              </Link>
-              <p className="text-[#57606a] text-sm">
-                {dayjs(change.date).format("MMMM DD, YYYY")}
-              </p>
+    <div className="ml-[50px] mr-[20px] flex justify-center">
+      <div className="max-w-[1024px]">
+        <p className="text-[32px] font-semibold pt-[50px] pb-[17px] border-b border-solid border-[#EAEAEA]">
+          Changelog
+        </p>
+        <div className="mt-8">
+          {changelog["changelog"].map((change) => (
+            <div
+              key={change.date}
+              className="flex flex-col sm:flex-row justify-between mb-12"
+            >
+              <div className="mr-12 flex-1 pb-8 sm:pb-0 h-fit sm:sticky top-[80px]">
+                <Link
+                  href={`/changelog/${change.name}`}
+                  className="text-2xl font-extrabold text-[#24292f]"
+                >
+                  {change.name}
+                </Link>
+                <p className="text-[#57606a] text-sm">
+                  {dayjs(change.date).format("MMMM DD, YYYY")}
+                </p>
+              </div>
+              <div className="grow-[2] lg:grow-[5] basis-0 flex justify-center">
+                <p className="">{change.description}</p>
+              </div>
             </div>
-            <div className="grow-[2] lg:grow-[5] basis-0 flex justify-center">
-              <p className="lg:w-[75%]">{change.description}</p>
-            </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </div>
   );
