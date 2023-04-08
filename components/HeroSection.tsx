70a12e3239fc5ed0b0949915e145fa0038aed12a
MarconLP <13001502+MarconLP@users.noreply.github.com>
2023-04-08 18:54:42 +0200

    update hero text and slogan

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -41,19 +41,21 @@ const button = {
 
 export default function HeroSection() {
   return (
-    <div className="text-center mt-[8rem]">
+    <div className="text-center mt-[4rem] flex justify-center">
       <motion.div
         variants={container}
         initial="hidden"
         animate="show"
-        className="flex flex-col items-center"
+        className="flex flex-col items-center max-w-[80%]"
       >
-        <motion.span variants={item}>Next Generation</motion.span>
         <motion.span
           variants={item}
-          className="title-text-gradient py-3 text-[12vw] sm:text-7xl font-black text-[#191919]"
+          className="title-text-gradient py-3 text-[10vw] leading-none md:text-7xl font-black text-[#191919]"
         >
-          Productivity
+          Open Management Infrastructure
+        </motion.span>
+        <motion.span variants={item} className="text-[#666]">
+          The open platform for seamless management and collaboration.
         </motion.span>
         <motion.div variants={button}>
           <Link href={process.env.NEXT_PUBLIC_FRONTEND_HOST}>
