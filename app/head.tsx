3c61263de965d08fa348d0a362f985216c100dbc
MarconLP <13001502+MarconLP@users.noreply.github.com>
2023-04-08 18:58:11 +0200

    update landing page title

--- app/head.tsx
+++ app/head.tsx
@@ -1,7 +1,7 @@
 export default function Head() {
   return (
     <>
-      <title>Tablane - Next generation Productivity.</title>
+      <title>Tablane | Open Management Infrastructure.</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta
         content="Tablane is a workspace that adapts to your needs. It's as minimal or as powerful as you need it to be."
